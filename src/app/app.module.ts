bde38fb4550e1f13cba42b156fdef85777f568f1
Lucas Santos <lukas@hotmail.co.jp>
2018-08-01 16:11:22 -0300

    divisao de conteudos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    NgbModule.forRoot(),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
